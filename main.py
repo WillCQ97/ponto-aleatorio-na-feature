ffcec744fa65fa29c7b37a1e25ea132605f0624c
WillCQ97 <willian.cqueiroz@gmail.com>
2023-11-16 11:04:08 -0300

    Codigo inicial gerado pelo chatGPT

---
+++ main.py
@@ -0,0 +1,22 @@
+import geopandas as gpd
+from shapely.geometry import shape, Point
+import random
+
+# Carregue o arquivo GeoJSON usando geopandas
+file_path = 'caminho/do/seu/arquivo.geojson'
+gdf = gpd.read_file(file_path)
+
+# Selecione uma geometria aleatória do GeoDataFrame
+random_index = random.randint(0, len(gdf) - 1)
+random_geometry = gdf['geometry'].iloc[random_index]
+
+# Gere um ponto aleatório dentro da geometria
+minx, miny, maxx, maxy = random_geometry.bounds
+while True:
+    x = random.uniform(minx, maxx)
+    y = random.uniform(miny, maxy)
+    point = Point(x, y)
+    if point.within(random_geometry):
+        break
+
+print(f'Ponto aleatório dentro da geometria: {point}')
