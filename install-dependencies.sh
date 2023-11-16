e7f4025476480c2e9cc918773c41d984f53367cf
WillCQ97 <willian.cqueiroz@gmail.com>
2023-11-16 13:49:42 -0300

    obtendo os dados a partir do geojson

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,1 +1,4 @@
+#!/bin/bash
+
+echo 'Instalando geopandas e shapely com o pip'
 pip install geopandas shapely
--- main.py
+++ main.py
@@ -2,28 +2,38 @@
     PROMPT 1: Como obter um ponto aleatório que esteja dentro de uma geometria, considerando que a feição esteja armazenada em um arquivo geojson?
     PROMPT 2: troque o código para ao invés de selecionar uma geometria randômica, obtenha um ponto aleatório para todas as feature de uma featurecollection
 '''
-
 import geopandas as gpd
 from shapely.geometry import shape, Point
 import random
 
-# Carregue o arquivo GeoJSON usando geopandas
-file_path = 'caminho/do/seu/arquivo.geojson'
-gdf = gpd.read_file(file_path)
-
-# Função para gerar um ponto aleatório dentro de uma geometria
 def generate_random_point(geometry):
+    # Gere um ponto aleatório dentro da geometria
     minx, miny, maxx, maxy = geometry.bounds
+    
     while True:
         x = random.uniform(minx, maxx)
         y = random.uniform(miny, maxy)
         point = Point(x, y)
-        if point.within(geometry):
+        
+        if point.within(geometry):  # exceção se a geometria for multipolygon
             return point
 
-# Adicione uma coluna 'random_point' ao GeoDataFrame com pontos aleatórios
-gdf['random_point'] = gdf['geometry'].apply(generate_random_point)
+# Carregue o arquivo GeoJSON usando geopandas
+file_path = 'geojsons/alegre_min.json'
+gdf = gpd.read_file(file_path)
 
-# Exiba o GeoDataFrame resultante
-print(gdf[['geometry', 'random_point']])
+# Selecione uma geometria aleatória do GeoDataFrame
 
+for index in range(0, len(gdf) - 1):
+
+    geometry = gdf['geometry'].iloc[index]
+    local_id = gdf['idd'].iloc[index]
+    local_pri = gdf['primario'].iloc[index]
+    local_sec = gdf['secundario'].iloc[index]
+
+    if geometry.geom_type == 'Polygon':
+        point = generate_random_point(geometry)
+
+        print(f'ID: {local_id} {local_pri}. Ponto aleatório gerado: {point}')
+    else:
+        print(f'ERRO! ID: {local_id} {local_pri}. Geometria: {geometry.geom_type}')
