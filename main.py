4f9bac70f584fb5306a8af9138dc7e17f0bedfba
WillCQ97 <willian.cqueiroz@gmail.com>
2023-11-17 23:27:10 -0300

    procenssando todos os geojsons para gerar um insert sql

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ import geopandas as gpd
 from shapely.geometry import shape, Point
 import random
 
+
 def generate_random_point(geometry):
     # Gere um ponto aleatório dentro da geometria
     minx, miny, maxx, maxy = geometry.bounds
-    
+
+    random.seed(1)
     while True:
         x = random.uniform(minx, maxx)
         y = random.uniform(miny, maxy)
@@ -18,22 +20,41 @@ def generate_random_point(geometry):
         if point.within(geometry):  # exceção se a geometria for multipolygon
             return point
 
-# Carregue o arquivo GeoJSON usando geopandas
-file_path = 'geojsons/alegre_min.json'
-gdf = gpd.read_file(file_path)
 
-# Selecione uma geometria aleatória do GeoDataFrame
+geojsons_unidades = {
+    1: 'alegre.json',
+    2: 'goiabeiras.json',
+    3: 'maruipe.json',
+    4: 'sao_mateus.json',
+    5: 'rive.json',
+    6: 'jeronimo_monteiro.json',
+#    8: 'sao_jose_do_calcado.geojson'
+}
 
-for index in range(0, len(gdf) - 1):
+print('INSERT INTO public.tb_locais(idd, primario, secundario, terciario, zona, localizacao, filename, id_unidade) VALUES ')
 
-    geometry = gdf['geometry'].iloc[index]
-    local_id = gdf['idd'].iloc[index]
-    local_pri = gdf['primario'].iloc[index]
-    local_sec = gdf['secundario'].iloc[index]
+for key, value in geojsons_unidades.items():
+    
+    # Carregue o arquivo GeoJSON usando geopandas
+    file_path = 'geojsons/' + value
+    gdf = gpd.read_file(file_path)
 
-    if geometry.geom_type == 'Polygon':
-        point = generate_random_point(geometry)
+    # Selecione uma geometria aleatória do GeoDataFrame
+    print()
+    for index in range(0, len(gdf) - 1):
 
-        print(f'ID: {local_id} {local_pri}. Ponto aleatório gerado: {point}')
-    else:
-        print(f'ERRO! ID: {local_id} {local_pri}. Geometria: {geometry.geom_type}')
+        local_id = gdf['idd'].iloc[index]
+        local_pri = gdf['primario'].iloc[index]
+        local_sec = gdf['secundario'].iloc[index]
+        local_ter = gdf['terciario'].iloc[index]
+        local_zona = gdf['zona'].iloc[index]
+
+        geometry = gdf['geometry'].iloc[index]
+        if geometry.geom_type == 'Polygon':
+            point = generate_random_point(geometry)
+
+        else:
+            point = 'null'
+
+        print(f'(\'{local_id}\', \'{local_pri}\', \'{local_sec}\', \'{local_ter}\', {local_zona}, \'{point}\', \'{value}\', \'{key}\')')
+
